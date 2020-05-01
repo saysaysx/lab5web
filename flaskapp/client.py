945f4d34fc28e4629c164c172ef4b7139a376217
saysaysx <63892671+saysaysx@users.noreply.github.com>
2020-05-01 17:48:07 +0700

    Add files via upload

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -17,10 +17,10 @@ with open(path, 'rb') as fh:
     b64 = base64.b64encode(img_data)
 
 
-jsondata = {'imagebin':b64.decode('utf-8')}
-res = requests.post('http://localhost:5000/apinet', json=jsondata)
-if res.ok:
-    print(res.json())
+#jsondata = {'imagebin':b64.decode('utf-8')}
+#res = requests.post('http://localhost:5000/apinet', json=jsondata)
+#if res.ok:
+#    print(res.json())
 
 try:
     r = requests.get('http://localhost:5000/apixml')
